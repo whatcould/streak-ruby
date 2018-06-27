551b9fb808cec3f6d9c019d14f1a2b5a26da7b61
austin <austin@dringo.io>
2018-06-27 12:13:17 -0700

    Added ability to add contacts to boxes

--- lib/streak/contact.rb
+++ lib/streak/contact.rb
@@ -6,6 +6,11 @@ module Streak
       extract_contacts_from obj
     end
 
+    def self.add_to_box(box_key, params={})
+      res = Streak.request(:post, "/boxes/" + box_key, MultiJson.dump(params))
+      obj = convert_to_streak_object(res, Contact)
+    end
+
     private
 
       def self.extract_contacts_from(obj)
