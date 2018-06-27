ae11c18244758e950421db7eb4469c5be88335ed
austin <austin@dringo.io>
2018-06-27 11:33:29 -0700

    Added method to extract contacts from results

--- lib/streak/contact.rb
+++ lib/streak/contact.rb
@@ -2,7 +2,14 @@ module Streak
   class Contact < StreakObject
     def self.get(params = {})
       res = Streak.request(:get, "/search?query=#{params[:email]}")
-      convert_to_streak_object(res, Contact)
+      obj = convert_to_streak_object(res, Contact)
+      extract_contact_from obj
     end
+
+    private
+
+      def self.extract_contacts_from(obj)
+        obj.instance_variable_get(:@values)[:results].instance_variable_get(:@values)[:contacts]
+      end
   end
 end
