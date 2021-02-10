79a90cb542f6b6e7562bbf4cfeeb9aa0c4d7b0be
David Reese <david@whatcould.com>
2021-02-10 16:00:02 -0500

    Update contact.rb
    Add Contact#find, Contact#create

--- lib/streak/contact.rb
+++ lib/streak/contact.rb
@@ -6,6 +6,16 @@ module Streak
       extract_contacts_from obj
     end
 
+    def self.find(key)
+      res = Streak.request_v2(:get, "/contacts/#{key}")
+      convert_to_streak_object(res, Contact)
+    end
+
+    def self.create(team_key, params={})
+      res = Streak.request_v2(:post, "/teams/#{team_key}/contacts/", MultiJson.dump(params))
+      convert_to_streak_object(res, Contact)
+    end
+
     def self.add_to_box(box_key, params={})
       res = Streak.request(:post, "/boxes/" + box_key, MultiJson.dump(params))
       obj = convert_to_streak_object(res, Contact)
